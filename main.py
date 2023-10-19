ebd241622d642add0b8647d22e20d0027ffc4638
grzlz <guillermo.rdzlpz@gmail.com>
2023-10-19 17:12:42 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# 1. Crea un sistema que lleve datos de un API a una base de datos usando python (venv) y SQL.
+# 2. Documenta todo el proceso utilizando Git y Github.
+# 3. Credenciales de acceso a una base de datos en Postgres:
+#    -Usuario: icarus-dev;
+#    -Constraseña: pass-no-hackeable.
